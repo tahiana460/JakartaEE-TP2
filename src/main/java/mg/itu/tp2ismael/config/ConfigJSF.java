5a508a02797b6e231da00fa37e4684e7ff85a582
Tahiana <tahianarazakanome@gmail.com>
2024-04-08 17:55:18 +0300

    init

---
+++ src/main/java/mg/itu/tp2ismael/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tp2ismael.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author ASPIRE
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
